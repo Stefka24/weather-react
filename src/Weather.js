cf707fa9982cb5220bbb8ef2e94ddf97ff155813
Stefka24 <84857822+Stefka24@users.noreply.github.com>
2021-06-19 16:31:00 +0100

    add axios to show current weather in city

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,40 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("Edinburgh");
+  const [name, setName] = useState("Edinburgh");
+  const [currentTemp, setCurrentTemp] = useState(15);
+  const [feelsLike, setFeelsLike] = useState(14);
+  const [icon, setIcon] = useState("01d");
 
   function updateCity(event) {
     event.preventDefault();
     setCity(event.target.value);
   }
+  function showWeather(response) {
+    setName(response.data.name);
+    setCurrentTemp(Math.round(response.data.main.temp));
+    setFeelsLike(Math.round(response.data.main.feels_like));
+    setIcon(response.data.weather[0].icon);
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    alert(city);
+    let apiKey = "2bbfe2c83b5eba58ece5b7c5c691290a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
   }
-  return (
+  let weather = (
     <div className="Weather">
       <div className="Temperature">
         <h1>
           <div className="row">
-            <div className="col-6">Edinburgh</div>
+            <div className="col-6">{name}</div>
             <div className="col-6 todaysDegrees">
-              18 ℃
+              {currentTemp} ℃
               <img
-                src="http://openweathermap.org/img/wn/10d@2x.png"
+                src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
                 alt="weather"
                 className="weatherIcon"
               />
@@ -30,15 +43,24 @@ export default function Weather() {
         </h1>
         <h4>
           <div className="row">
-            <div className="col-6">9th June 10:20am</div>
-            <div className="col-6 todaysDegrees">Feels like 17℃</div>
+            <div className="col-6">19th June 10:20am</div>
+            <div className="col-6 todaysDegrees">Feels like {feelsLike}℃</div>
           </div>
         </h4>
       </div>
-      <form className="Search" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter a city" onChange={updateCity} />
-        <input type="submit" value="Search" className="searchButton" />
-      </form>
+    </div>
+  );
+
+  let form = (
+    <form className="Weather" onSubmit={handleSubmit}>
+      <input type="text" placeholder="Enter a city" onChange={updateCity} />
+      <input type="submit" value="Search" className="searchButton" />
+    </form>
+  );
+  return (
+    <div>
+      {weather}
+      {form}
     </div>
   );
 }
