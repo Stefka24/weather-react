dcfc37b84f03cf4f7b48791aff0ab246f09f098a
Stefka24 <84857822+Stefka24@users.noreply.github.com>
2021-06-19 12:47:20 +0100

    added weather app project components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import "./App.css";
-import axios from "axios";
+import Search from "./Search";
+import Temperature from "./Temperature";
 
-function App(props) {
-  function showTemperature(response) {
-    alert(
-      `The temperature in ${response.data.name} is ${response.data.main.temp}`
-    );
-  }
-  let apiKey = "2bbfe2c83b5eba58ece5b7c5c691290a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div className="App card framework">
+      <div className="card-body">
+        <Temperature />
+        <Search />
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App city="Paris" />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
