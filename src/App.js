5a818b578dcabdfbf793d3d43b7ed5f196de394b
Stefka24 <84857822+Stefka24@users.noreply.github.com>
2021-06-20 11:25:25 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App card framework">
-      <div className="card-body">
-        <Weather />
+    <div className="App">
+      <div className="container">
+        <div className="card framework">
+          <div className="card-body">
+            <Weather />
+          </div>
+        </div>
       </div>
     </div>
   );
